cfcbc75746076f4128cba96735835c72753b7c7d
unknown <halifa.halisia@gmail.com>
2021-08-17 19:47:24 +0200

    fix java buf

--- backend/java/planning/src/main/plugins/SamplePluginImp.java
+++
@@ -1,9 +0,0 @@
-package plugins;
-
-public class SamplePluginImp implements Plugin {
-    @Override
-    public boolean load() {
-        System.out.println("SamplePlugin loaded");
-        return true;
-    }
-}
